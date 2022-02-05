41843afa18dddf9f1052a03bb34fb72ebd1bfc75
exwendjs <X4vier@23>
2022-02-05 18:41:48 -0300

    update: update sql script

--- coletaAgro/bd.sql
+++ coletaAgro/bd.sql
@@ -6,7 +6,7 @@ id serial primary key,
 nome_comum varchar(255) not null,
 nome_cientifico varchar(255) not null,
 situacao varchar(255) not null,
-num_registro date not null,
+num_registro varchar(50),
 data_registro date not null,
 requerente varchar(500)
 );
