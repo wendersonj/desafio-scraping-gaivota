06ef745e5728303b54074d40df9b0d010babc6cf
exwendjs <X4vier@23>
2022-02-05 18:45:01 -0300

    feat: incremented the pipeline to save data in the database

--- coletaAgro/coletaAgro/pipelines.py
+++ coletaAgro/coletaAgro/pipelines.py
@@ -7,19 +7,24 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
+from .DAO.CultivaresDAO import CultivaresDB
 from .items import CultivarItem
 from .utils.utils import convert_str_to_date
 
 
 class CultivarPipeline:
+    db = CultivaresDB()
 
     def open_spider(self, spider):
-        pass
+        self.db.create_connection()
 
     def close_spider(self, spider):
-        pass
+        self.db.close_connection()
 
     def process_item(self, item: CultivarItem, spider):
         item['data_registro'] = convert_str_to_date(item['data_registro'])
+
+        self.db.insert_data(sql_data=[item['cultivar'], item['nome_comum'], item['nome_cientifico'], item['situacao'],
+                                      item['num_registro'], item['data_registro'], item['requerente']])
 
         return item
