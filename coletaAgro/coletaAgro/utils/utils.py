68d62cdda8c4a531e029e9ab117a4262cb3ebcc9
exwendjs <X4vier@23>
2022-02-05 13:50:39 -0300

    fix: fix function return

--- coletaAgro/coletaAgro/utils/utils.py
+++ coletaAgro/coletaAgro/utils/utils.py
@@ -2,4 +2,4 @@ from datetime import date, datetime
 
 
 def convert_str_to_date(value: str) -> date:
-    return date(datetime.strptime(value, '%d/%m/%Y'))
+    return datetime.strptime(value, '%d/%m/%Y').date()
