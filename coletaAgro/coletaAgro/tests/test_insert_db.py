5b94b69457022484e9a4c28ba329a5debcdbd287
exwendjs <X4vier@23>
2022-02-07 07:47:25 -0300

    feat: new pytest

---
+++ coletaAgro/coletaAgro/tests/test_insert_db.py
@@ -0,0 +1,28 @@
+from datetime import date
+from itertools import zip_longest
+
+from ..DAO.CultivaresDAO import CultivaresDB
+from ..items import CultivarItem
+from ..spiders.cultiver_web_spider import get_shell_base
+
+'''
+use pytest
+'''
+
+
+def test_insert_item_in_db():
+    db = CultivaresDB()
+    db.create_connection()
+    item = ['Arroz', 'Arrozinho',
+            'Arroz de verdade',
+            'regular', '8002',
+            date(9999, 1, 1),
+            'wenderson']
+
+    item = dict(zip_longest(get_shell_base(), item))
+
+    result = CultivarItem.insert_data(db=db, sql_data=[item['cultivar'], item['nome_comum'],
+                                                       item['nome_cientifico'], item['situacao'],
+                                                       item['num_registro'], item['data_registro'],
+                                                       item['requerente']])
+    assert result
